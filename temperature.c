47801c97ba0dbf5b5a7c976d32e41debb0850df1
salavthapa <salav.thapa4@gmail.com>
2016-11-23 10:32:19 +0545

    Add files via upload

---
+++ temperature.c
@@ -0,0 +1,25 @@
+/*
+student Name:Salav Thapa
+subject:Programing fundamental
+Roll No:
+Program:To read temperature in fahrenhit
+Lab Sheet No:5
+Date:2073-08-8
+*/
+
+#include<stdio.h>
+#include<math.h>
+int main(){
+    float c,f;
+    printf("enter the temperature in fahrenhit\n");
+    scanf("%f",&f);
+    c=(0.5*(f-32));
+    printf("enter the temperature in centigrade is %.2f",c);
+    return 0;
+}
+
+
+
+
+
+
