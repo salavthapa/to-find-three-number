acdd02a7f1870613cda740b91af52f975dd19d4f
salavthapa <salav.thapa4@gmail.com>
2016-11-23 10:18:20 +0545

    Add files via upload

---
+++ to find three digit number.c
@@ -0,0 +1,24 @@
+/*
+Student Name:Salav Thapa
+Subject:Programing Fundamental
+Roll No:
+Program:To find sum of three digit number
+Lab Sheet No:5
+Date:2073-08-8
+*/
+
+#include<stdio.h>
+#include<math.h>
+int main(){
+    float sum,a,b,c;
+    printf("enter the first digit number\n");
+    scanf("%f",&a);
+    printf("enter the second number\n");
+    scanf("%f",&b);
+    printf("enter the third digit number\n");
+    scanf("%f",&c);
+    sum=a+b+c;
+    printf("the sum of three digit number%.2f",sum);
+    return 0;
+
+}
